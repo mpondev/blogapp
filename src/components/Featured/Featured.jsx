4a35800d1bd2ea74efbb65356ad32e1c346eb252
mpondev <mario@mpondev.com>
2023-09-11 10:26:38 +0200

    Featured post design

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,7 +1,30 @@
 import styles from './Featured.module.scss';
 
 function Featured() {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, mpondev here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imageContainer}>
+          <img src="/p1.jpeg" alt="image1" />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+            temporibus necessitatibus voluptatum excepturi et reprehenderit,
+            veniam id voluptate. Totam porro doloribus repellendus saepe,
+            incidunt molestiae excepturi alias accusantium. Atque, tempora?
+          </p>
+          <button className={styles.postButton}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Featured;

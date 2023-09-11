d8471534875644a8b4bc8723137963551fe49759
Mario Ponce <mario@mpondev.com>
2023-09-11 16:38:09 +0200

    Pagination buttons design

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,7 +1,12 @@
 import styles from './Pagination.module.scss';
 
 function Pagination() {
-  return <div className={styles.container}>Pagination</div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.btn}>Previous</button>
+      <button className={styles.btn}>Next</button>
+    </div>
+  );
 }
 
 export default Pagination;
